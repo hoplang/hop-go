13f75ecef7a866a617c2ebfbd39cc88881e47b7a
Anton Lyxell <git@lyxell.dev>
2025-04-15 11:23:07 +0200

    refactor: Bump nixos

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4989a246d7a390a859852baddb1013f825435cee.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d2141817e8e9083ad4338f9a8020cadfb9729648.tar.gz") {} }:
 
 pkgs.mkShell {
 	nativeBuildInputs = [
